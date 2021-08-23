dfd6bb04869dcf215d04d15a1b34dfab988b0ae7
이재환 <qorvhfl080@naver.com>
2021-08-23 14:55:47 +0900

    메세지 어댑터 뼈대

---
+++ app/src/main/java/com/neppplus/numberbaseballgame_20210823/adapter/MessageAdapter.kt
@@ -0,0 +1,32 @@
+package com.neppplus.numberbaseballgame_20210823.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.neppplus.numberbaseballgame_20210823.R
+import com.neppplus.numberbaseballgame_20210823.data.Message
+
+class MessageAdapter(val mContext: Context, val resId: Int, mList: ArrayList<Message>) : ArrayAdapter<Message>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.message_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+
+
+        return row
+    }
+
+}
