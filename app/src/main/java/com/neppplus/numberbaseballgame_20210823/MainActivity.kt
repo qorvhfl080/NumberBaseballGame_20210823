798e17dc724db6c83f8f284713e5f2b7f64a4b71
이재환 <qorvhfl080@naver.com>
2021-08-23 14:58:33 +0900

    연습용 메세지 데이터 추가

--- app/src/main/java/com/neppplus/numberbaseballgame_20210823/MainActivity.kt
+++ app/src/main/java/com/neppplus/numberbaseballgame_20210823/MainActivity.kt
@@ -2,10 +2,23 @@ package com.neppplus.numberbaseballgame_20210823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.neppplus.numberbaseballgame_20210823.adapter.MessageAdapter
+import com.neppplus.numberbaseballgame_20210823.data.Message
 
 class MainActivity : AppCompatActivity() {
+
+    val messageList = ArrayList<Message>()
+
+    lateinit var messageAdapter: ArrayAdapter<MessageAdapter
+            >
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        messageList.add(Message("안녕하세요", "CPU"))
+        messageList.add(Message("반갑습니다", "USER"))
+
     }
 }
