1a20d7ec4200aa929203dc514ccfd0e468082020
이재환 <qorvhfl080@naver.com>
2021-08-23 15:03:53 +0900

    리스트뷰의 getView UI 찾아오기

--- app/src/main/java/com/neppplus/numberbaseballgame_20210823/adapter/MessageAdapter.kt
+++ app/src/main/java/com/neppplus/numberbaseballgame_20210823/adapter/MessageAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.neppplus.numberbaseballgame_20210823.R
 import com.neppplus.numberbaseballgame_20210823.data.Message
@@ -23,6 +24,11 @@ class MessageAdapter(val mContext: Context, val resId: Int, val mList: ArrayList
         val row = tempRow!!
 
         val data = mList[position]
+
+        val cpuMessageLayout = row.findViewById<LinearLayout>(R.id.cpuMessageLayout)
+        val userMessageLayout = row.findViewById<LinearLayout>(R.id.userMessageLayout)
+        val cpuMessageTxt = row.findViewById<TextView>(R.id.cpuMessageTxt)
+        val userMessageTxt = row.findViewById<TextView>(R.id.userMessageTxt)
 
         return row
     }
